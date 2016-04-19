8b9e620564a1d830990fd39ef0e63d9c9ac31034
Morrie Cunningham <cunningm@mail.gvsu.edu>
2016-04-19 17:00:28 -0400

    Code Portion of Project = Done

--- Matrix.swift
+++ Matrix.swift
@@ -1,7 +1,40 @@
-//: Playground - noun: a place where people can play
+/**********************************************
+ *
+ * Matrix.swift
+ *
+ * Matrix Operations in Swift
+ * 
+ * CIS 343 - Project 6
+ * Professor Nandigam
+ *
+ * Authors: Morrie Cunningham & Devin Brown
+ *
+ ***********************************************/
 
 import Cocoa
 
+// Created our own errors
+enum MatrixError : ErrorType {
+    case InvalidRows
+    case InvalidColumns
+    case InvalidMultiply
+}
+
+// Creates the messages for our errors
+extension MatrixError : CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .InvalidRows:
+            return "Rows Don't Match!"
+        case .InvalidColumns:
+            return "Columns Don't Match!"
+        case .InvalidMultiply:
+            return "The number of columns of Matrix A doesnt equal the rows of Matrix B!"
+        }
+    }
+}
+
+// Main Matrix class
 class Matrix: NSObject {
     
     var columns: Int = 0
@@ -25,6 +58,7 @@ class Matrix: NSObject {
         self.init(rows: 5, columns: 5, data: 0)
     }
     
+    // Constructor function
     init(rows: Int, columns: Int, data: Int) {
         self.rows = rows
         self.columns = columns
@@ -36,16 +70,27 @@ class Matrix: NSObject {
         }
     }
     
+    // Getter method
     func get(row: Int, column: Int) -> Int {
         return self.data[(row*self.columns) + column]
     }
     
+    // Setter method
     func set(row: Int, column: Int, val: Int) {
         self.data[(row*self.columns) + column] = val
     }
     
-    func add(m: Matrix) -> Matrix{
+    // Adds two Matrices together
+    func add(m: Matrix) throws -> Matrix{
         let newM = Matrix.init(rows: self.rows, columns: self.columns, data: 0)
+        
+        if self.rows != m.rows {
+            throw MatrixError.InvalidRows
+        }
+        
+        if self.columns != m.columns {
+            throw MatrixError.InvalidRows
+        }
         
         for r in 0..<rows {
             for c in 0..<columns {
@@ -56,8 +101,17 @@ class Matrix: NSObject {
         return newM
     }
     
-    func subtract(m: Matrix) -> Matrix{
+    // Subtracts two Matrices from one another
+    func subtract(m: Matrix) throws -> Matrix{
         let newM = Matrix.init(rows: self.rows, columns: self.columns, data: 0)
+        
+        if self.rows != m.rows {
+            throw MatrixError.InvalidRows
+        }
+        
+        if self.columns != m.columns {
+            throw MatrixError.InvalidRows
+        }
         
         for r in 0..<rows {
             for c in 0..<columns {
@@ -68,6 +122,7 @@ class Matrix: NSObject {
         return newM
     }
     
+    // Multiplies a Matrix by an integer
     func scalarMult(m: Matrix, k: Int) -> Matrix{
         let newM = Matrix.init(rows: self.rows, columns: self.columns, data: 0)
         
@@ -80,8 +135,13 @@ class Matrix: NSObject {
         return newM
     }
     
-    func Multiply(m: Matrix) -> Matrix{
+    // Multiplies two Matrices together
+    func Multiply(m: Matrix) throws -> Matrix{
         let newM = Matrix.init(rows: self.rows, columns: m.columns, data: 0)
+        
+        if self.columns != m.rows {
+            throw MatrixError.InvalidMultiply
+        }
         
         for r in 0..<rows {
             for c2 in 0..<m.columns {
@@ -95,7 +155,8 @@ class Matrix: NSObject {
         
         return newM
     }
-
+    
+    // Transposes the given Matrix
     func Transpose(m: Matrix) -> Matrix{
         let newM = Matrix.init(rows: self.columns, columns: self.rows, data: 0)
         
@@ -108,6 +169,7 @@ class Matrix: NSObject {
         return newM
     }
     
+    // Creates an Identity Matrix of a given size
     static func indentity(size: Int) -> Matrix {
         let newM = Matrix.init(rows: size, columns: size, data: 0)
         
@@ -122,10 +184,12 @@ class Matrix: NSObject {
         return newM
     }
     
+    // Function to fill Matrix with a value
     func fill(val: Int) {
         self.data = [Int](count: self.data.count , repeatedValue: val)
     }
     
+    // Clones a Matrix
     func clone() -> Matrix {
         let newM = Matrix.init(rows: self.rows, columns: self.columns, data: 0)
         newM.data = self.data    // Swift array so this is deep copy
@@ -133,11 +197,13 @@ class Matrix: NSObject {
         return newM
     }
     
+    // Function to print a Matrix
     func generate() -> AnyGenerator<Int> {
-        // keep the index of the next car in the iteration
+        // keep the index of the next cell value in the iteration
         var nextIndex = self.data.count-1
         
-        // Construct a AnyGenerator<Car> instance, passing a closure that returns the next car in the iteration
+        // Construct a AnyGenerator<Int> instance, passing a closure that returns the 
+        // next cell value in the iteration
         return AnyGenerator {
             if (nextIndex < 0) {
                 return nil
@@ -149,19 +215,22 @@ class Matrix: NSObject {
     
 }
 
-
-func +(m1: Matrix, m2: Matrix) -> Matrix {
-    return m1.add(m2)
+// Function to override the '+' operand to add Matrices
+func +(m1: Matrix, m2: Matrix) throws -> Matrix {
+    return try m1.add(m2)
 }
 
-func -(m1: Matrix, m2: Matrix) -> Matrix {
-    return m1.subtract(m2)
+// Function to override the '-' operand to subtract Matrices
+func -(m1: Matrix, m2: Matrix) throws-> Matrix {
+    return try m1.subtract(m2)
 }
 
-func *(m1: Matrix, m2: Matrix) -> Matrix {
-    return m1.Multiply(m2)
+// Function to override the '*' operand to multiply Matrices
+func *(m1: Matrix, m2: Matrix) throws -> Matrix {
+    return try m1.Multiply(m2)
 }
 
+// Function to see if two Matrices are equal to each other
 func ==(m1: Matrix, m2: Matrix) -> Bool {
     for r in 0..<m1.rows {
         for c in 0..<m1.columns {
@@ -172,8 +241,3 @@ func ==(m1: Matrix, m2: Matrix) -> Bool {
     }
     return true
 }
-
-
-
-
-
--- main.swift
+++ main.swift
@@ -1,30 +1,67 @@
-//
-//  main.swift
-//  CIS343MatrixOperations
-//
-//  Created by Morrie Cunningham on 4/9/16.
-//  Copyright © 2016 Morrie Cunningham. All rights reserved.
-//
+/**********************************************
+ *
+ * main.swift
+ *
+ * Main Method for Matrix Operations in Swift
+ *
+ * CIS 343 - Project 6
+ * Professor Nandigam
+ *
+ * Authors: Morrie Cunningham & Devin Brown
+ *
+ ***********************************************/
 
 import Foundation
 
+// Creating some matrices to test with
 var m1 = Matrix.init(rows: 3, columns: 4, data: 4)
 var m2 = Matrix.init(rows: 3, columns: 4, data: 2)
 var m3 = Matrix.init(rows: 4, columns: 5, data: 3)
 var m4 = Matrix.init(rows: 3, columns: 5, data: 40)
 var m5 = Matrix.init(rows: 3, columns: 5, data: 40)
 
-
+// Tests printing the matrices
 print(m1)
-print(m3)
-print(m1 * m3)
+print(m2)
 print(m3)
 print(m4)
-print(m1.add(m3))
-print(m1.subtract(m2))
+
+// Tests the error of when multiplying 2 invalid matrices together
+do {
+    try print(m1 * m2)
+} catch MatrixError.InvalidMultiply {
+    print("Error: \(MatrixError.InvalidMultiply.description)\n")
+}
+
+// Tests the error of when adding 2 invalid matrices together
+do {
+    try print(m1.add(m3))
+    
+} catch MatrixError.InvalidRows {
+    print("Error: \(MatrixError.InvalidRows.description)\n")
+} catch MatrixError.InvalidColumns {
+    print("Error: \(MatrixError.InvalidColumns.description)\n")
+}
+
+// Tests the error of when subtracting 2 invalid matrices together
+do {
+    try print(m1.subtract(m2))
+} catch MatrixError.InvalidRows {
+    print("Error: \(MatrixError.InvalidRows.description)")
+} catch MatrixError.InvalidColumns {
+    print("Error: \(MatrixError.InvalidColumns.description)")
+}
+
+// Tests the scalar multiply by a matrix function
 print(m1.scalarMult(m1, k: 7))
+
+// Tests the transposing function
 print(m4.Transpose(m4))
+
+// Tests the identity matrix function
 print(Matrix.indentity(7))
+
+// Tests the comparable function if 2 matrices are equal to each other
 print("Is Matrix 1 equal to Matrix 2?? \(m1 == m2)")
 print("Is Matrix 4 equal to Matrix 5?? \(m4 == m5)")
 
